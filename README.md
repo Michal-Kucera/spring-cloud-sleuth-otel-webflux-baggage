e3869f6840d3ba5870e10bee4773de40949688af
Michal Kucera <michal.kucera@payxpert.com>
2022-08-18 15:24:54 +0200

    Add first approach as per https://github.com/spring-projects-experimental/spring-cloud-sleuth-otel/issues/122#issue-1336941175

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spring-cloud-sleuth-otel-webflux-baggage
-The purpose of this repository is to reproduce [Baggage is not propagated to MDC](https://github.com/spring-projects-experimental/spring-cloud-sleuth-otel/issues/122) issue in [spring-cloud-sleuth-otel](https://github.com/spring-projects-experimental/spring-cloud-sleuth-otel) library
+
+The purpose of this repository is to
+reproduce [Baggage is not propagated to MDC](https://github.com/spring-projects-experimental/spring-cloud-sleuth-otel/issues/122)
+issue in [spring-cloud-sleuth-otel](https://github.com/spring-projects-experimental/spring-cloud-sleuth-otel) library
+
+# How to run it?
+
+- Compile and run with `maven`
+- Hit [http://localhost:8080](http://localhost:8080) endpoint
+- Have a look at logs in console
+
+> **Note:** There is another branch, which uses the second approach, with `WebFluxSleuthOperators` utility class.
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.SpanCustomizer;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class DemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/")
+    public String home() {
+        logger.info("Handling home");
+        return "Hello World";
+    }
+}
+
+@Configuration
+class UserIdWebFilter implements WebFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserIdWebFilter.class);
+
+    private static final String USER_ID = "userId";
+
+    private final Tracer tracer;
+
+    public UserIdWebFilter(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return Mono.just(generateRandomUserId())
+                .doOnNext(this::addUserIdTag)
+                .doOnNext(this::addUserIdBaggage)
+                .then(chain.filter(exchange));
+    }
+
+    private String generateRandomUserId() {
+        return UUID.randomUUID().toString();
+    }
+
+    // TODO: this is not working for reactor's threads
+    private void addUserIdBaggage(String userId) {
+        tracer.createBaggage(USER_ID, userId);
+        logger.debug("User ID baggage added");
+    }
+
+    private void addUserIdTag(String userId) {
+        SpanCustomizer spanCustomizer = tracer.currentSpanCustomizer();
+        if (spanCustomizer != null) {
+            spanCustomizer.tag(USER_ID, userId);
+        }
+    }
+}
